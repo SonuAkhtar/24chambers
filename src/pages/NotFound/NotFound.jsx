4939ac54761365e800aa3ea7b2ceef9e13a85e12
SonuA <sonua981@gmail.com>
2024-10-13 17:04:21 +0530

    notfound page complete

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
+// import CSS
+
 import "./notFound.css";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="notfound_wrapper">
+      <div className="notfound_big">404</div>
+
+      <p className="notfound_info">
+        <span>404</span> Sorry, we couldn't find this page.
+      </p>
+
+      <Link to="/">
+        Go to homepage <i className="fa-solid fa-house" />
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
