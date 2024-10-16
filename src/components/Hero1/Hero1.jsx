46d2b3c2b5a9e97ccccad1850ed6900118889c69
SonuA <sonua981@gmail.com>
2024-10-16 14:39:13 +0530

    hero section updated

--- src/components/Hero1/Hero1.jsx
+++ src/components/Hero1/Hero1.jsx
@@ -2,7 +2,28 @@
 import "./hero1.css";
 
 const Hero1 = () => {
-  return <section className="hero1_section"></section>;
+  return (
+    <section className="hero1_section">
+      <div className="hero1_wrapper">
+        <h1 className="hero_title">24 Chambers: Your Trusted Legal Partner </h1>
+        <div className="hero_subtitle">
+          <p>
+            A law firm dedicated to delivering exceptional legal expertise and
+            personalized service to clients. With a team of seasoned lawyers, 24
+            Chambers provides tailored solutions backed by extensive knowledge
+            and experience, ensuring clients receive the best possible
+            representation.
+          </p>
+
+          <p>
+            With the Head Office in New Delhi and a commitment to serving
+            clients across India, 24 Chambers is well-positioned to navigate the
+            legal landscape.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero1;
