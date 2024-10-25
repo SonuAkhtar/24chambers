a0b2e84dbeb3192480a9218e69a1db2a368826d2
SonuA <sonua981@gmail.com>
2024-10-26 00:31:19 +0530

    hero section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,46 +5,27 @@ const Hero = () => {
   return (
     <section className="hero_section">
       <div className="hero_wrapper">
-        <h1 className="hero_title one">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title two">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title three">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title four">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title five">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title six">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title seven">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title eight">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
-        <h1 className="hero_title nine">
-          24 Chambers: Your Trusted Legal Partner
-        </h1>
+        <h1 className="hero_title">24 Chambers: Your Trusted Legal Partner</h1>
         <div className="hero_subtitle">
           <p>
-            A law firm dedicated to delivering exceptional legal expertise and
-            personalized service to clients. With a team of seasoned lawyers, 24
-            Chambers provides tailored solutions backed by extensive knowledge
-            and experience, ensuring clients receive the best possible
-            representation.
-          </p>
-
-          <p>
-            With the Head Office in New Delhi and a commitment to serving
-            clients across India, 24 Chambers is well-positioned to navigate the
-            legal landscape.
+            <span>
+              24 Chambers is a law firm dedicated to delivering exceptional
+              legal expertise and personalized service to clients across a range
+              of core practice areas. With its Head Office in New Delhi, the
+              firm is well- positioned to serve clients across India and is
+              well-positioned to navigate the legal landscape.
+            </span>
+            <span>
+              With a team of seasoned lawyers, 24 Chambers provides tailored
+              solutions backed by extensive knowledge and experience, ensuring
+              clients receive the best possible representation.
+            </span>
+            <span>
+              We believe in building strong relationships based on trust,
+              communication, and dedication to achieving our clients&#39; goals.
+              Our team works with clients to develop and implement customised
+              legal strategies to secure the best possible outcomes.
+            </span>
           </p>
         </div>
       </div>
