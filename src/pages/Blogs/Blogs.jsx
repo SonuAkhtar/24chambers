1b96c026a2aa61f5ce08bbcd04e02f7c596eb584
SonuA <sonua981@gmail.com>
2024-10-28 12:17:33 +0530

    blogs section updated

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -4,7 +4,9 @@ import "./blogs.css";
 const Blogs = () => {
   return (
     <div className="blogs">
-      <div className="blogs_wrapper">BLogs</div>
+      <div className="blogs_wrapper">
+        <main>Blogs section coming soon...</main>
+      </div>
     </div>
   );
 };
